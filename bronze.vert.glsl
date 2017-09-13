dc4843b10b07d2ea042de64fcd51050714248f95
davidtu2 <david.tu2@csu.fullerton.edu>
2017-09-13 04:14:28 -0700

    added bronze shaders and readme file

---
+++ bronze.vert.glsl
@@ -0,0 +1,15 @@
+# version 120
+// These are passed in from the CPU program
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+// These are variables that we wish to send to our fragment shader
+// In later versions of GLSL, these are 'out' variables.
+varying vec3 myNormal;
+varying vec4 myVertex;
+
+void main() {
+  gl_Position = projectionMatrix * modelViewMatrix * gl_Vertex;
+  myNormal = gl_Normal;
+  myVertex = gl_Vertex;
+}
